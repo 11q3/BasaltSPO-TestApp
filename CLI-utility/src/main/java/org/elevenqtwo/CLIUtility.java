cb5a282e5a3803f8c5185c8fa6574214a0e31628
elevenqtwo <elevenqtwo@gmail.com>
2024-04-12 21:33:04 +0300

    Repository, package, maven sctructure

--- CLI-utility/src/main/java/org/elevenqtwo/CLIUtility.java
+++ CLI-utility/src/main/java/org/elevenqtwo/CLIUtility.java
@@ -1,6 +1,6 @@
-package org.example;
+package org.elevenqtwo;
 
-public class Main {
+public class CLIUtility {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ PackageComparator/src/main/java/org/elevenqtwo/PackageComparator.java
@@ -0,0 +1,7 @@
+package org.elevenqtwo;
+
+public class PackageComparator {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
