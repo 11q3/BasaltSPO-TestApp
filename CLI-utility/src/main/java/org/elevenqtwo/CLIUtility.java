48bf4405a75db7911211fd430ac2bcc1984f9c0f
elevenqtwo <elevenqtwo@gmail.com>
2024-04-15 04:01:07 +0300

    CLI starting point

--- CLI-utility/src/main/java/org/elevenqtwo/CLIUtility.java
+++ CLI-utility/src/main/java/org/elevenqtwo/CLIUtility.java
@@ -1,7 +1,29 @@
 package org.elevenqtwo;
 
+import org.elevenqtwo.service.PackageComparator;
+import org.elevenqtwo.service.PackageFetcher;
+import org.elevenqtwo.util.JsonWriter;
+
 public class CLIUtility {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 3) {
+            System.err.println("Usage: java " +
+                    "-jar PackageComparator-1.0-SNAPSHOT.jar " +
+                    "<branch1> <branch2> <outputFilePath>");
+            return;
+        }
+
+        String branch1 = args[0];
+        String branch2 = args[1];
+        String filePath = args[2];
+
+        PackageFetcher packageFetcher = new PackageFetcher();
+        PackageComparator packageComparator = new PackageComparator();
+
+        JsonWriter.writeJSONToFile(
+                packageComparator.comparePackages(
+                        packageFetcher.fetchPackages(branch1),
+                        packageFetcher.fetchPackages(branch2)),
+                filePath);
     }
 }
